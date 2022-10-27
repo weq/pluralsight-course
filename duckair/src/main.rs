8c3b61a52f5d11dafb5206fafb54ee7d689868a0
weq <weq+github@weq.no>
2022-10-27 20:40:32 +0200

    Added module 3 and corrected something in module 2

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -2,5 +2,7 @@
 #![allow(unused_variables)]
 
 fn main() {
-    println!("Hello, world!");
+    let location: (&str, f64, f64) = ("KCLE", 41.4094069, -81.8546911);
+    let (name, latitude, longitude) = location;
+    println!("Location name: {}, latitude: {}, longitude: {}", name, latitude, longitude);
 }
